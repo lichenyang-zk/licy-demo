d01ba3d710020ac0968be214c9cbf8e2276ff339
lichenyang-zk <18336547612@163.com>
2022-07-05 23:25:01 +0800

    licy-demo初始化

---
+++ boot-demo/src/main/java/cn/licy/demo/controller/DemoController.java
@@ -0,0 +1,25 @@
+package cn.licy.demo.controller;
+
+import cn.licy.demo.service.DemoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * DemoController
+ *
+ * @author lichenyang
+ * @version 1.0.0 2022/7/5 22:58
+ */
+@RestController
+public class DemoController {
+
+    @Resource
+    private DemoService demoService;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return demoService.hello();
+    }
+}
