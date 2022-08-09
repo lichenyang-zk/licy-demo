3b1725b57f1f1932c11e8373513bdc130c8e024a
lichenyang-cs <17516787612@163.com>
2022-08-09 16:47:45 +0800

    sso-client01初始化

---
+++ oauth2-client01-demo/src/main/java/com/xxx/lict/client/Client01Application.java
@@ -0,0 +1,17 @@
+package com.xxx.lict.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author lichenyang
+ * @since 2022/8/8 15:56
+ */
+@SpringBootApplication
+public class Client01Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Client01Application.class, args);
+    }
+}
