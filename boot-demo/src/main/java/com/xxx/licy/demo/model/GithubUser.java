40365a8abc9c19b64fdcb03f5b5f8b050a5f5ddd
lichenyang-zk <18336547612@163.com>
2022-10-13 22:33:46 +0800

    github用户信息类

---
+++ boot-demo/src/main/java/com/xxx/licy/demo/model/GithubUser.java
@@ -0,0 +1,10 @@
+package com.xxx.licy.demo.model;
+
+/**
+ * 通过 https://api.github.com/search/users?q=获取的GitHub用户信息，将获取的信息，封装到该对象中
+ *
+ * @author lichenyang
+ * @version 1.0.0 2022/9/25 20:18
+ */
+public class GithubUser {
+}
