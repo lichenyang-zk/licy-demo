490e30ba7d1dcddbd01a43e2f56b9a5bffabc284
licy <18336547612@163.com>
2024-08-29 17:35:51 +0800

    excel-demo init.

---
+++ excel-demo/src/main/java/com/xxx/licy/excel/ExcelApplication.java
@@ -0,0 +1,10 @@
+package com.xxx.licy.excel;
+
+/**
+ * 启动类
+ *
+ * @author lichenyang
+ * @since 2024/8/29 17:23
+ */
+public class ExcelApplication {
+}
---
+++ excel-demo/src/main/java/com/xxx/licy/excel/util/ExcelUtil.java
@@ -0,0 +1,10 @@
+package com.xxx.licy.excel.util;
+
+/**
+ * Excel工具类
+ *
+ * @author lichenyang
+ * @since 2024/8/29 17:27
+ */
+public class ExcelUtil {
+}
